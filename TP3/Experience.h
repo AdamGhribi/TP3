bcd37bc06ba33cd87072a49532b18518e58c32bb
AdamGhribi <adam_ghribi@outlook.com>
2022-02-28 17:43:09 +0100

    Add project files.

---
+++ TP3/Experience.h
@@ -0,0 +1,13 @@
+#ifndef EXPERIENCE_H
+#define EXPERIENCE_H
+// à  faire : classe Interface
+class Experience
+{
+public:
+	virtual ~Experience() = default;
+	virtual void afficher() const = 0;
+	virtual unsigned int obtenirExperience() const = 0;
+	virtual string obtenirNomExperience() const = 0;
+
+};
+#endif
